784e998a04ab200f7b5a200243bacefcd91eefbe
Yanvi Soni <yanvi5900@gmail.com>
2021-05-19 23:37:08 +0530

    First commit

---
+++ website/index.php
@@ -0,0 +1,126 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Reserve Bank</title>
+	<link rel="stylesheet" href="bootstrap-4.1.3-dist/css/bootstrap.min.css">
+	<link rel="stylesheet" href="style.css">
+	<link rel="stylesheet" href="css/fixed.css">
+	<link href="https://fonts.googleapis.com/css2?family=Nunito+Sans:ital,wght@1,300&family=Ubuntu:ital@1&display=swap" rel="stylesheet">
+	<link rel="preconnect" href="https://fonts.gstatic.com">
+	<link href="https://fonts.googleapis.com/css2?family=Righteous&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Dancing+Script&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Zilla+Slab:ital@1&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Bree+Serif&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Amiri&display=swap" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css2?family=Cardo&display=swap" rel="stylesheet">
+</head>
+
+<body data-spy="scroll" data-target="#navbarResponsive">
+	<!--Start of home section-->
+	<div id="home">
+  <!--Navigation-->
+	<nav class="navbar navbar-expand-md navbar-dark bg-dark fixed-top">
+		<a style="font-family:Bree Serif; font-size:31px" class="navbar-brand"href="index.php">Sparks Bank</a>
+		<button class="navbar-toggler"type="button"data-toggle="collapse"data-target="#navbarResponsive">
+    <span class="navbar-toggler-icon"></span>
+		</button>
+   <div class="collapse navbar-collapse"id="navbarResponsive">
+		 <ul class="navbar-nav ml-auto">
+     <li class="nav-item">
+			 <a class="nav-link"href="index.php" style="font-family:Zilla Slab;">Home</a>
+		 </li>
+		 <li class="nav-item">
+			 <a class="nav-link"href="https://www.thesparksfoundationsingapore.org/" style="font-family:Zilla Slab;">About Us</a>
+		 </li>
+		 <li class="nav-item">
+			 <a class="nav-link"href="#Contact" style="font-family:Zilla Slab;">Contact</a>
+		 </li>
+		 </ul>
+	 </div>
+	</nav>
+	<!--Start Image Slider-->
+	<div id="carouselExampleIndicators"class="carousel slide"data-ride="carousel"data-interval="7000">
+		<ol class="carousel-indicators">
+			<li data-target="#carouselExampleIndicators" data-slide-to="0"class="active"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+			<li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+		</ol>
+		<div class="carousel-inner"role="listbox">
+			<div class="carousel-item active"style="background-image: url('https://arizent.brightspotcdn.com/dims4/default/7ef9045/2147483647/strip/true/crop/570x395+0+0/resize/570x395!/quality/90/?url=https%3A%2F%2Fsource-media-brightspot.s3.amazonaws.com%2F50%2F54%2Fef810056450ebf60db74f082f95a%2Fp17lhvl3u915jbbdhoc9ffgcss7.jpg');">
+				<div class="carousel-caption text-center">
+					<h5 style="font-family:Algerian; font-size:4rem; color:white;">Welcome to Sparks Bank!</h5>
+				  <br>
+					<br>
+					</div>
+		</div>
+			<div class="carousel-item"style="background-image: url('https://www.incimages.com/uploaded_files/image/1920x1080/getty_158673029_9707279704500119_78594.jpg');"></div>
+				<div class="carousel-item"style="background-image: url('https://t3.ftcdn.net/jpg/02/57/26/40/360_F_257264006_3ZmALSn60tWA8RCEA2PoNUvrW0mJQhsK.jpg');"></div>
+				<div class="carousel-item"style="background-image: url('https://arizent.brightspotcdn.com/dims4/default/7ef9045/2147483647/strip/true/crop/570x395+0+0/resize/570x395!/quality/90/?url=https%3A%2F%2Fsource-media-brightspot.s3.amazonaws.com%2F50%2F54%2Fef810056450ebf60db74f082f95a%2Fp17lhvl3u915jbbdhoc9ffgcss7.jpg');"></div>
+				<a class="carousel-control-prev"href="#carouselExampleIndicators" role="button"data-slide="prev">
+        <span class="carousel-control-prev-icon"aria-hidden="true"></span>
+				</a>
+				<a class="carousel-control-next"href="#carouselExampleIndicators" role="button"data-slide="next">
+        <span class="carousel-control-next-icon"aria-hidden="true"></span>
+				</a>
+	</div>
+</div>
+<section class="services" class="offset">
+                <h2 id="services1"></h2>
+                        <div class="s-heading">
+                        <h1 style="font-family:Zilla Slab;">OUR SERVICES</h1 >
+                        <p style="font-family:Zilla Slab;">Enjoy the best services provided by our bank!!</p>
+                				</div>
+                <div class="s-box-container" style="font-family:Amiri;">
+
+                        <div class="s-box">
+
+                                <div class="bar"></div>
+                                <img src ="img/user.png" alt="1">
+                                <h1 style="font-family:Amiri;">View users</h1>
+                                <a href="view_user.php" class="s-btn" >View</a>
+                        </div>
+
+                         <div class="s-box">
+                                <div class="bar"></div>
+                                <img src ="img/transfer.png" alt="2">
+                                <h1 style="font-family:Amiri;">Transfer Money</h1>
+                                <a href="moneytransfer.php" class="s-btn" >View</a>
+                        </div>
+                                <div class="s-box">
+                                <div class="bar"></div>
+                                <img src ="img/transaction.png" alt="3">
+                                <h1 style="font-family:Amiri;">Transactions</h1>
+                                <a href="transactionhistory.php" class="s-btn" >View</a>
+                        </div>
+                </div>
+							</section>
+
+<div id="Contact" class="offset">
+	<footer>
+		<div class="row justify-content-center">
+			<div class="col-md-5 text-center">
+				<p style="font-size:2rem; text-decoration: underline; text-align:center; font-family: Cardo;">Sparks Bank</p>
+				<p style="font-family:Cardo;">The most trustworthy Bank</p>
+				<strong style="font-family:Cardo;">Contact Info</strong>
+				<p style="font-family:Cardo;">+91-9825000000
+					<br>
+					</p>
+					<a href="https://www.facebook.com/thesparksfoundation.info"><i class="fab fa-facebook-square"></i></a>
+					<a href="https://twitter.com/tsfsingapore"><i class="fab fa-twitter-square"></i></a>
+					<a href="https://www.instagram.com/thesparksfoundation.info"><i class="fab fa-instagram"></i></a>
+			</div>
+			<hr class="socket" style="font-family:Cardo;">
+			&copy; Sparks Bank
+		</div>
+	</footer>
+</div>
+<!--- Script Source Files -->
+<script src="js/jquery-3.3.1.min.js"></script>
+<script src="bootstrap-4.1.3-dist/js/bootstrap.min.js"></script>
+<script src="https://use.fontawesome.com/releases/v5.6.1/js/all.js"></script>
+<!--- End of Script Source Files -->
+
+</body>
+</html>
